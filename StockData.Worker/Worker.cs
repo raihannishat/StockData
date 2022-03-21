5b20d2980ec0d79fcb185b002089f5c190923342
raihannishat <raihannishat.swe@gmail.com>
2022-03-21 15:25:00 +0600

    Worker Service Layer

---
+++ StockData.Worker/Worker.cs
@@ -0,0 +1,44 @@
+using Autofac;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using StockData.Scraping.Services;
+
+namespace StockData.Worker
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        public ILifetimeScope AutofacContainer { get; private set; }
+
+        public Worker(ILogger<Worker> logger, ILifetimeScope lifetimeScope)
+        {
+            _logger = logger;
+            AutofacContainer = lifetimeScope;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var scrapingService = AutofacContainer.Resolve<IScrapingService>();
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    scrapingService.SaveStockData();
+                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogInformation(ex, DateTimeOffset.Now.ToString());
+                }
+
+                await Task.Delay(60000, stoppingToken);
+            }
+        }
+    }
+}
