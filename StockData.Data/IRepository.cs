5f0d5e69fa48eed124ae49edb0891ead265cbf5d
raihannishat <raihannishat.swe@gmail.com>
2022-03-21 15:20:50 +0600

    Data Access Layer

---
+++ StockData.Data/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace StockData.Data
+{
+    public interface IRepository<TEntity, TKey> where TEntity : IEntity<TKey>
+    {
+        void AddAll(IList<TEntity> entities);
+    }
+}
