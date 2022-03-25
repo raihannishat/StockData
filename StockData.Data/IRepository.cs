c52d21cb78e5945a2f2fb8467c240e02efec9097
raihannishat <raihannishat.swe@gmail.com>
2022-03-25 09:41:02 +0600

    Data Access Layer

--- StockData.Data/IRepository.cs
+++ StockData.Data/IRepository.cs
@@ -8,5 +8,14 @@ namespace StockData.Data
     public interface IRepository<TEntity, TKey> where TEntity : IEntity<TKey>
     {
         void AddAll(IList<TEntity> entities);
+        IList<TEntity> GetAll();
+        IList<TEntity> GetAll(Expression<Func<TEntity, bool>> filter);
+        (IList<TEntity> data, int total, int totalDisplay) GetDynamic(
+            Expression<Func<TEntity, bool>> filter = null,
+            string orderBy = null,
+            string includeProperties = "", 
+            int pageIndex = 1, 
+            int pageSize = 10, 
+            bool isTrackingOff = false);
     }
 }
--- StockData.Data/Repository.cs
+++ StockData.Data/Repository.cs
@@ -23,5 +23,64 @@ namespace StockData.Data
         {
             _dbSet.AddRange(entities);
         }
+
+        public IList<TEntity> GetAll(Expression<Func<TEntity, bool>> filter)
+        {
+            IQueryable<TEntity> query = _dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            return query.ToList();
+        }
+
+        public IList<TEntity> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public virtual (IList<TEntity> data, int total, int totalDisplay) GetDynamic(
+            Expression<Func<TEntity, bool>> filter = null,
+            string orderBy = null,
+            string includeProperties = "",
+            int pageIndex = 1, 
+            int pageSize = 10, 
+            bool isTrackingOff = false)
+        {
+            IQueryable<TEntity> query = _dbSet;
+            var total = query.Count();
+            var totalDisplay = query.Count();
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+                totalDisplay = query.Count();
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                var result = query.OrderBy(orderBy).Skip((pageIndex - 1) * pageSize).Take(pageSize);
+                if (isTrackingOff)
+                    return (result.AsNoTracking().ToList(), total, totalDisplay);
+                else
+                    return (result.ToList(), total, totalDisplay);
+            }
+            else
+            {
+                var result = query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+                if (isTrackingOff)
+                    return (result.AsNoTracking().ToList(), total, totalDisplay);
+                else
+                    return (result.ToList(), total, totalDisplay);
+            }
+        }
     }
 }
